dd6bf79170177af37c19f75c0b6a90e2aa874e57
ssserchenko <se.volchenko@ya.ru>
2023-03-29 03:48:43 +0300

    Homework 2 fix

--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -3,7 +3,11 @@ package ru.tinkoff.edu.java.scrapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import reactor.core.publisher.Mono;
+import ru.tinkoff.edu.java.scrapper.client.StackOverflowClient;
+import ru.tinkoff.edu.java.scrapper.client.dto.response.StackOverflowQuestionsResponse;
 import ru.tinkoff.edu.java.scrapper.configuration.ApplicationConfig;
+import ru.tinkoff.edu.java.scrapper.client.dto.request.StackOverflowQuestionRequest;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
@@ -12,5 +16,10 @@ public class ScrapperApplication {
         var ctx = SpringApplication.run(ScrapperApplication.class, args);
         ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
         System.out.println(config);
+
+//        StackOverflowClient stackOverflowClient = ctx.getBean(StackOverflowClient.class);
+//        Mono<StackOverflowQuestionsResponse> response =
+//                stackOverflowClient.getQuestionInfoFromClient(new StackOverflowQuestionRequest(30917782));
+//        System.out.println(response.block());
     }
 }
--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/ApplicationConfig.java
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/ApplicationConfig.java
@@ -2,11 +2,19 @@ package ru.tinkoff.edu.java.scrapper.configuration;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.annotation.Validated;
-import ru.tinkoff.edu.java.scrapper.sheduler.Scheduler;
+import ru.tinkoff.edu.java.scrapper.scheduler.Scheduler;
 
 @Validated
 @EnableScheduling
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record ApplicationConfig(@NotNull String test, @NotNull Scheduler scheduler) {}
+public record ApplicationConfig(@NotNull String test, @NotNull Scheduler scheduler) {
+
+    @Bean
+    public String delay() {
+        return scheduler().interval().toString();
+    }
+
+}
