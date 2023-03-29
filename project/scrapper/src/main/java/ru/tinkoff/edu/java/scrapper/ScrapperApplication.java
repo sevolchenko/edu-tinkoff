9de60f25deb4ceeba210048f16b248b134bec84c
ssserchenko <se.volchenko@ya.ru>
2023-03-29 18:23:21 +0300

    Homework 2 full fixed

--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -3,11 +3,7 @@ package ru.tinkoff.edu.java.scrapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import reactor.core.publisher.Mono;
-import ru.tinkoff.edu.java.scrapper.client.StackOverflowClient;
-import ru.tinkoff.edu.java.scrapper.client.dto.response.StackOverflowQuestionsResponse;
 import ru.tinkoff.edu.java.scrapper.configuration.ApplicationConfig;
-import ru.tinkoff.edu.java.scrapper.client.dto.request.StackOverflowQuestionRequest;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
@@ -16,10 +12,5 @@ public class ScrapperApplication {
         var ctx = SpringApplication.run(ScrapperApplication.class, args);
         ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
         System.out.println(config);
-
-//        StackOverflowClient stackOverflowClient = ctx.getBean(StackOverflowClient.class);
-//        Mono<StackOverflowQuestionsResponse> response =
-//                stackOverflowClient.getQuestionInfoFromClient(new StackOverflowQuestionRequest(30917782));
-//        System.out.println(response.block());
     }
 }
