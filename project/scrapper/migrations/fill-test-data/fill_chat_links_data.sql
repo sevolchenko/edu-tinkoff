b5995d0af1041949a423a90d966b21aad042f5e0
Sergey Volchenko <se.volchenko@ya.ru>
2023-04-18 22:04:58 +0300

    Homework 4 fix after comments 2

--- project/scrapper/migrations/fill-test-data/fill_chat_links_data.sql
+++
@@ -1,7 +0,0 @@
-
-insert into chat_links (chat_id, link_id)
-    values (48319238913, 1);
-insert into chat_links (chat_id, link_id)
-    values (5613212, 2);
-insert into chat_links (chat_id, link_id)
-    values (48319238913, 2);
