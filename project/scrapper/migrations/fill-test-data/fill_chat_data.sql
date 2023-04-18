f433c4714802ccbf29ff0360e3b2e3e363156ca9
Sergey Volchenko <se.volchenko@ya.ru>
2023-04-18 22:04:58 +0300

    Homework 4 fix after comments 2

--- project/scrapper/migrations/fill-test-data/fill_chat_data.sql
+++
@@ -1,7 +0,0 @@
-
-insert into chat (id, username)
-    values (48319238913, 'Igor');
-insert into chat (id, username)
-    values (5613212, 'Bot');
-insert into chat (id, username)
-    values (8974533, 'Kate');
---
+++ project/scrapper/migrations/init-db/create_tg_chat_table.sql
@@ -0,0 +1,11 @@
+-- Create Tables
+-- ================================================================
+-- Document
+create table tg_chat
+(
+    tg_chat_id    bigint       not null,
+    username      varchar(255) not null,
+    registered_at timestamptz  not null,
+
+    constraint tg_chat_pk primary key (tg_chat_id)
+);
