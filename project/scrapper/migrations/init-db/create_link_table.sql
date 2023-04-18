a102a17237dd1ceaf78b279ad85bdf831bd39019
Sergey Volchenko <se.volchenko@ya.ru>
2023-04-18 13:59:39 +0300

    Homework 4 fix after comments

--- project/scrapper/migrations/init-db/create_link_table.sql
+++ project/scrapper/migrations/init-db/create_link_table.sql
@@ -8,7 +8,7 @@ create sequence link_sequence start with 1 increment by 1;
 create table link
 (
     id         integer      not null default nextVal('link_sequence'),
-    url        varchar(255) not null,
+    url        varchar(255) not null unique,
     scanned_at timestamptz  not null,
 
     constraint link_pk primary key (id)
